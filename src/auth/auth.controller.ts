a08e085ce1f56d68d6857279d025dfc8dae05cba
loicfarel <loicfarele@gmail.com>
2024-07-24 17:33:57 +0100

    path  auth/me

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dto/login.dto';
 import { Public } from './decorators/public.decorator';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from 'src/user/entities/user.entity';
 
 @ApiTags('Authentification')
-@Public()
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -15,11 +16,19 @@ export class AuthController {
     private readonly userService: UserService,
   ) {}
 
+  @ApiBearerAuth()
+  @Get('me')
+  async me(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @Public()
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     return await this.userService.create(createUserDto);
   }
 
+  @Public()
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto);
